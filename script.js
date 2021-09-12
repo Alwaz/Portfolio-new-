2daaf220944fe879cc49d87361726476cd7bc991
Alwaz <alwazkazi3@gmail.com>
2021-09-12 12:32:22 -0700

    logo for light mode fixed

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ let toggle_btn = document.querySelector(".slider");
 let element = document.body;
 let nav = document.querySelector(".navbar");
 
+// responsive menu bar
+let menu = document.querySelector("#menu-bar");
+
+// menu bar
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  nav.classList.toggle("active");
+});
+
+// dark light mode toggle
 toggle_btn.addEventListener("click", () => {
   element.classList.toggle("dark-mode");
   nav.classList.toggle("dark-mode");
+  icon.classList.toggle("dark-mode");
 });
